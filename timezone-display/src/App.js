0c01073e3ed715fc95409e1638ad7c5164553dad
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2021-09-14 12:26:45 -0700

    Add prop to determine bg
    Issues with props not working properly, only works sometimes and not every time.

--- timezone-display/src/App.js
+++ timezone-display/src/App.js
@@ -1,18 +1,53 @@
 
 import './App.css';
+import { useState, useEffect } from 'react';
 
 import Clock from './components/Clock';
-import ClockDark from './components/ClockDark';
+// import ClockDark from './components/ClockDark';
 
 
 function App() {
+
+  const API_KEY = "95VKVKO1R2B0";
+
+  const [time, setTime] = useState([]);
+
+  useEffect(() => {
+
+    // getLocation();
+    getTime();
+
+  }, []);
+
+  // const getLocation = () => (
+
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+
+  //     let lat = position.coords.latitude;
+  //     let long = position.coords.longitude;
+  
+  //   }));
+
+
+    const getTime = async () => {
+
+      const response = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=${API_KEY}&format=json&by=position&lat=52.3555&lng=1.1743`);
+  
+      const data = await response.json();
+      
+      setTime(data);
+
+    }
+
+    // getLocation();
+
   return (
     <div className="App">
      
 
-    {/* <Clock /> */}
+    <Clock time={time}/>
 
-    <ClockDark />
+    {/* <ClockDark /> */}
 
     </div>
   );
--- timezone-display/src/components/Clock.jsx
+++ timezone-display/src/components/Clock.jsx
@@ -1,23 +1,27 @@
 import styled from "styled-components";
 
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
+// import { CollectionsBookmarkOutlined } from "@material-ui/icons";
+
 
 const Container = styled.div`
 
-    background-image: url("https://images.unsplash.com/photo-1445561696415-deadc6a2adaa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80");
+    background-image: url(${(props)=>props.bg < "12" ? "https://images.unsplash.com/photo-1445561696415-deadc6a2adaa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80" : "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"});
+
     background-size: cover;
     height: 100vh;
 
     display: flex;
     justify-content: center;
 
-
 `;
+
+
 
 const Wrapper = styled.div`
 
     background-color: #ccc;
-    background-color:rgba(224,224,224, 0.4);
+    background-color: rgba(224,224,224, 0.4);
     color: #000;
     display: flex;
     flex-direction: column;
@@ -69,9 +73,30 @@ const Timezone = styled.h2`
 
 const Location = styled.h3``;
 
-const Clock = () => {
+const Clock = ({time}) => {
+
+
+    const splitTime = (convertTime) => {
+
+        let timeConvert = convertTime;
+
+        let timesplit = timeConvert.split(' ');
+
+        
+        let time24 = timesplit[1].split(":");
+
+        const time = time24[0] + ":" + time24[1];
+        
+        console.log(time);
+
+        return time;
+
+    }
+
+    const time24 = splitTime(time.formatted);
+
     return (
-        <Container>
+        <Container bg={time24}>
             <Wrapper>
                 <Title> 
                     <Icons>
@@ -80,10 +105,10 @@ const Clock = () => {
                     Good Morning, It's Currently
                     </Title>
                     <TimeContainer>
-                        <Time>11:37</Time>
-                        <Timezone> BST</Timezone>
+                        <Time>{time24}</Time>
+                        <Timezone> { time.nextAbbreviation}</Timezone>
                     </TimeContainer>
-                <Location>In London, UK</Location>
+                <Location>TimeZone: {time.zoneName}</Location>
                 </Wrapper>
         </Container>
     )
